0a6e93e19efff383d99cd2042fe8775f9275b61d
masum <masumcit44@gmail.com>
2023-05-02 09:34:21 +0600

    functionality added to access all receipe

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const chef = require('./data/chef.json')
+const receipe = require('./data/receipe.json')
 app.get("/",(req,res)=>{
     res.send("food lover is running ")
 })
 
 app.get("/chef",(req,res)=>{
     res.send(chef)
+})
+app.get("/receipe",(req,res)=>{
+    res.send(chef)
+})
+app.get("/receipe/:id",(req,res)=>{
+    const id = req.params.id;
+    const selectedReceipe = receipe.find(single=> single.id == id )
+    res.send(selectedReceipe)
 })
 
 
