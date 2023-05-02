60ec2f563b25056a3c0fe6943299bc0a1b39e8b3
masum <masumcit44@gmail.com>
2023-05-02 09:31:02 +0600

    receipe added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000;
-
+const chef = require('./data/chef.json')
 app.get("/",(req,res)=>{
     res.send("food lover is running ")
+})
+
+app.get("/chef",(req,res)=>{
+    res.send(chef)
 })
 
 
